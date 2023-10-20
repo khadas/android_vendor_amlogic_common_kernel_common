907c65820d8172c8814c5da1ab0c230a6ef305da
Xiong Zhang <xiong.zhang@wesion.com>
2023-10-20 09:59:23 +0800

    phy/realtek: fixup WOL on shutdown
    Signed-off-by: Xiong Zhang <xiong.zhang@wesion.com>

--- drivers/net/ethernet/stmicro/stmmac/dwmac-meson.c
+++ drivers/net/ethernet/stmicro/stmmac/dwmac-meson.c
@@ -27,6 +27,7 @@
 #include <linux/amlogic/cpu_version.h>
 #endif
 #include "stmmac_platform.h"
+#include <linux/amlogic/cpu_version.h>
 
 #define ETHMAC_SPEED_10	BIT(1)
 
@@ -612,6 +613,7 @@ static int meson6_dwmac_resume(struct device *dev)
 }
 EXPORT_SYMBOL_GPL(meson6_dwmac_resume);
 
+void rtl8211f_shutdown(void);
 void meson6_dwmac_shutdown(struct platform_device *pdev)
 {
 	struct net_device *ndev = platform_get_drvdata(pdev);
@@ -638,6 +640,7 @@ void meson6_dwmac_shutdown(struct platform_device *pdev)
 		}
 		dwmac_meson_disable_analog(&pdev->dev);
 	}
+
 	//stmmac_release(ndev);
 	//stmmac_pltfr_suspend(&pdev->dev);
 	if (priv->phydev) {
@@ -649,6 +652,9 @@ void meson6_dwmac_shutdown(struct platform_device *pdev)
 		}
 	}
 	stmmac_pltfr_suspend(&pdev->dev);
+
+	if (is_meson_gxm_cpu() || is_meson_g12a_cpu() || is_meson_g12b_cpu())
+		rtl8211f_shutdown();
 }
 
 #endif
